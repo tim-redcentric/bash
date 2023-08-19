7af90071689df6ed3451554cca85cf52edcf1d99
tim-redcentric <133682411+tim-redcentric@users.noreply.github.com>
2023-08-19 23:30:19 +0100

    Update clone-all-repos.sh

--- clone-all-repos.sh
+++ clone-all-repos.sh
@@ -1,1 +1,14 @@
-while read -r repo; do git clone "https://github.com/$repo.git"; done < <(gh repos | grep -oE 'Kaplan-Singapore/.+\s+')
+#!/bin/bash
+
+# Tim Samanchi - RedcentricPLC
+# 17/08/2023
+
+# gh and gitsome need to be installed before using this script
+# chmod +x clone-all-repos.sh
+# ./clone-all-repos.sh 
+
+organization="YourOrganizationName"  # Replace with your organization name, example: Kaplan-Singapore
+
+while read -r repo; do 
+    git clone "https://github.com/$repo.git"; 
+done < <(gh repos | grep -oE "$organization/.+\s+")
